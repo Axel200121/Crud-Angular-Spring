5f76fb53bf40244f58ec6060183e15ed2a259083
Axel200121 <axelmartinez212001@gmail.com>
2023-07-28 15:45:04 -0600

    Importando registros a la bd

---
+++ spring-boot-backend-apirest/src/main/resources/import.sql
@@ -0,0 +1,7 @@
+INSERT INTO cliente (nombre, apellido, email, create_at) VALUES ('John', 'Doe', 'john.doe@example.com', '2023-07-01');
+INSERT INTO cliente (nombre, apellido, email, create_at) VALUES ('Jane', 'Smith', 'jane.smith@example.com', '2023-07-02');
+INSERT INTO cliente (nombre, apellido, email, create_at) VALUES ('Michael', 'Johnson', 'michael.johnson@example.com', '2023-07-03');
+INSERT INTO cliente (nombre, apellido, email, create_at) VALUES ('Emily', 'Williams', 'emily.williams@example.com', '2023-07-04');
+INSERT INTO cliente (nombre, apellido, email, create_at) VALUES ('James', 'Brown', 'james.brown@example.com', '2023-07-05');
+INSERT INTO cliente (nombre, apellido, email, create_at) VALUES ('Sarah', 'Davis', 'sarah.davis@example.com', '2023-07-20');
+INSERT INTO cliente (nombre, apellido, email, create_at) VALUES ('Axel', 'Martinez', 'axel.dev@example.com', '2023-07-20');
