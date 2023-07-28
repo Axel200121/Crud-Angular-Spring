53aa81b7fedb4c64a845855ace9c6b603aafe565
Axel200121 <axelmartinez212001@gmail.com>
2023-07-28 16:23:52 -0600

    Agregando CRUD en service

--- spring-boot-backend-apirest/src/main/java/com/ams/springbootbackendapirest/models/services/ClienteServiceImpl.java
+++ spring-boot-backend-apirest/src/main/java/com/ams/springbootbackendapirest/models/services/ClienteServiceImpl.java
@@ -4,20 +4,40 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.ams.springbootbackendapirest.models.doa.IClienteDao;
 import com.ams.springbootbackendapirest.models.entity.Cliente;
 
-
 @Service
 public class ClienteServiceImpl implements IClienteService {
-	
-	//inyectamos con Autowired
+
+	// inyectamos con Autowired
 	@Autowired
 	private IClienteDao clienteDao;
+
 	@Override
+	@Transactional(readOnly = true)
 	public List<Cliente> findAll() {
 		return (List<Cliente>) clienteDao.findAll(); // TODO: mandamos a llamar el metodo
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public Cliente findById(Long id) {
+		return clienteDao.findById(id).orElse(null);
+	}
+
+	@Override
+	@Transactional
+	public Cliente save(Cliente cliente) {
+		return clienteDao.save(cliente);
+	}
+
+	@Override
+	@Transactional
+	public void delete(Long id) {
+		clienteDao.deleteById(id);
 	}
 
 }
