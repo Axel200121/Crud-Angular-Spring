a94ee085e21a13aa640e9c7f769db679920d8681
Axel200121 <axelmartinez212001@gmail.com>
2023-07-28 12:20:45 -0600

    Componente listado de clientes

---
+++ frontal/src/app/clientes/clientes.json.ts
@@ -0,0 +1,14 @@
+import { Cliente } from "./cliente";
+
+export const CLIENTES: Cliente[] = [
+    {id:1,  nombre: 'Axel', apellido: 'Martinez', email: 'ams@gmail.com', createAt:'2023-07-07'},
+    {id:2,  nombre: 'Axel', apellido: 'Martinez', email: 'ams@gmail.com', createAt:'2023-07-07'},
+    {id:3,  nombre: 'Axel', apellido: 'Martinez', email: 'ams@gmail.com', createAt:'2023-07-07'},
+    {id:4,  nombre: 'Axel', apellido: 'Martinez', email: 'ams@gmail.com', createAt:'2023-07-07'},
+    {id:5,  nombre: 'Axel', apellido: 'Martinez', email: 'ams@gmail.com', createAt:'2023-07-07'},
+    {id:5,  nombre: 'Axel', apellido: 'Martinez', email: 'ams@gmail.com', createAt:'2023-07-07'},
+    {id:7,  nombre: 'Axel', apellido: 'Martinez', email: 'ams@gmail.com', createAt:'2023-07-07'},
+    {id:8,  nombre: 'Axel', apellido: 'Martinez', email: 'ams@gmail.com', createAt:'2023-07-07'},
+    {id:9,  nombre: 'Axel', apellido: 'Martinez', email: 'ams@gmail.com', createAt:'2023-07-07'},
+    {id:10,  nombre: 'Axel', apellido: 'Martinez', email: 'ams@gmail.com', createAt:'2023-07-07'},
+  ];
