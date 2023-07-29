e7762a0a11e5d99465c158fc77400599cdcd482b
Axel200121 <axelmartinez212001@gmail.com>
2023-07-28 19:43:23 -0600

    Diseño UI y consumiendo enpoitn save

--- frontal/src/app/clientes/services/cliente.service.ts
+++ frontal/src/app/clientes/services/cliente.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Cliente } from '../cliente';
 import { CLIENTES } from '../clientes.json';
 import { Observable, of } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 //este decorador son para los servicios
 @Injectable({
@@ -11,10 +11,16 @@ import { HttpClient } from '@angular/common/http';
 export class ClienteService {
 
  private urlEndPoint: string = 'http://localhost:8080/api'; 
+ private httpHeaders = new HttpHeaders({'Content-Type': 'application/json'})
 
   constructor(private http: HttpClient) { }
 
   getClientes(): Observable<Cliente[]>{
     return this.http.get<Cliente[]>(this.urlEndPoint+'/clientes/find-all')
   }
+
+  createCliente(cliente:Cliente): Observable<Cliente>{
+    return this.http.post<Cliente>(this.urlEndPoint+'/clientes/save', cliente, {headers:this.httpHeaders});
+  }
+  
 }
